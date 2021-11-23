0fce468bb08090c46eff131f2c6a7dee5a52b688
Umesh <92439041+Umeshchaurasiya123@users.noreply.github.com>
2021-11-23 18:29:28 +0530

    Add files via upload

---
+++ dev folder/array.js
@@ -0,0 +1,93 @@
+//array decleration
+//let arr=[] 
+//let a=[]
+//let umesh=[]
+/*
+let arr=[1,2,3,4]
+console.log(arr)
+*/
+//*** in javascript our array can contain more then 1 type of data type
+// but index alwase start from zero
+//example
+
+//let arr1=[1,'hello',3.0,true]
+//console.log(arr1)
+//---------------------------------------------------
+//array methods
+//let car=['bmw','nano','honda city']
+//console.log(car)
+// to replace an element in an array
+//our index start from 0
+//car[1]='alto'
+//car[4]='suzuki'
+//console.log(car)
+//output [ 'bmw', 'alto', 'honda city', <1 empty item>, 'suzuki' ]
+
+//now to add new element into array 
+//here our car array does not have index 3 but we are adding here
+//car[3]='xux700'
+//console.log(car)
+//output  [ 'bmw', 'alto', 'honda city', 'xux700', 'suzuki' ]
+
+//now compute the length of an array
+/*
+let arr=['us','india','belgium','franse']
+console.log(arr.length)
+//output 4
+*/
+//------------------------------------------------------------------------
+//Array Manipulation Methods
+
+//pop() it takes out elemet from last index of an ayyay
+/*
+let country=['a','b','c','d']
+console.log(country.pop())
+console.log(country.pop())
+console.log(country)
+//output
+d
+c
+[ 'a', 'b' ]
+
+*/
+/*
+//push() it add eleemt at the end of an array
+let country=['a','b','c','d']
+country.push('e')
+country.push('f')
+//country.push('g')
+console.log(country)
+//output [ 'a', 'b', 'c', 'd', 'e', 'f' ]
+*/
+/*
+//to remove an eleemt at the start of an array
+//shift();
+let country=['a','b','c','d']
+console.log(country.shift())
+console.log(country.shift())
+console.log(country)
+//output
+a
+b
+[ 'c', 'd' ]
+
+*/
+
+/*
+
+//.unshift() to add eleemtn at the start of an Array
+
+let country=['a','b','c','d']
+console.log(country.unshift('2'))
+console.log(country.unshift('1'))
+console.log(country.unshift('0'))
+console.log(country)
+
+//output [
+  '0', '1', '2',
+  'a', 'b', 'c',
+  'd'
+]
+*/
+
+
